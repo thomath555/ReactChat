a286bc9309dc3014a61e956e774aadfcb1cb25c5
Thomas George <thomath47@hotmail.com>
2019-04-27 15:45:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { MessageList, MessageInput } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = new StreamChat('fpgkvdheujjw');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiam9sbHktYm9udXMtMyJ9.XoZ7Pkzit4GnKgYM1HI8T8ewxwqHQaDiJrnV9DT2EFg';
+
+chatClient.setUser(
+  {
+       id: 'jolly-bonus-3',
+       name: 'Thomas George',
+       image: 'https://content.thriveglobal.com/wp-content/uploads/2018/12/profile21.jpg'
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'godevs', {
+  // add as many custom fields as you'd like
+  image: 'https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png',
+  name: 'Talk about Go',
+});
+
+const App = () => (
+  <div className="wrapper">
+    <Chat client={chatClient} theme={'messaging light'}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  </div>
+);
 
 export default App;
